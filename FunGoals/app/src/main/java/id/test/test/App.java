7edd5bb2bad6ab7eb0659b987d8f36b6d1be8f4f
RexSeizen <seizenB@gmail.com>
2024-05-30 10:28:08 +0800

    refactor: modify App.java

--- FunGoals/app/src/main/java/id/test/test/App.java
+++ FunGoals/app/src/main/java/id/test/test/App.java
@@ -3,11 +3,13 @@
  */
 package id.test.test;
 
+import id.test.test.Database.DatabaseManager;
 import id.test.test.Scenes.LoginScene;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class App extends Application {
     // Metode utama untuk meluncurkan aplikasi JavaFX
@@ -18,18 +20,20 @@ public class App extends Application {
     // Metode start adalah titik masuk untuk aplikasi JavaFX
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Set the icon
+        Image appIcon = new Image(getClass().getResourceAsStream("/img/logo-fungoals.jpg"));
+        primaryStage.getIcons().add(appIcon);
+
+        DatabaseManager.initializeDatabase();
 
         // Mendapatkan batas visual dari layar utama
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 
-
         // Membuat instance dari LoginScene, dan melewatkan primary stage
         LoginScene loginScene = new LoginScene(primaryStage);
 
-
         // Mengatur scene dari primary stage ke login scene
         primaryStage.setScene(loginScene.getScene());
-
 
         // Mengatur lebar dan tinggi dari primary stage agar sesuai dengan batas layar
         primaryStage.setWidth(screenBounds.getWidth());
@@ -37,8 +41,7 @@ public class App extends Application {
 
         // Memaksimalkan primary stage agar mengambil seluruh layar
         primaryStage.setMaximized(true);
-        
+
         primaryStage.show();
     }
-
 }
